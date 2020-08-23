8729778cc3e3b108855c8a81e0ba508324c4048f
possible-gratitude <abayomi.gratitudeindia@gmail.com>
2020-08-23 18:08:48 +0100

    style file adjusted

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Header extends Component {
-  render() {
-    return (
-      <header className="top">
-        <h1>
-          Catch
-          <span className="ofThe">
-            <span className="of">Of</span>
-            <span className="the">The</span>
-          </span>
-          Day
-        </h1>
-        <h3>
-          <span>Seafood Market </span>
-        </h3>
-      </header>
-    );
-  }
-}
+const Header = (props) => {
+  return (
+    <header className="top">
+      <h1>
+        Catch
+        <span className="ofThe">
+          <span className="of">Of</span>
+          <span className="the">The</span>
+        </span>
+        Day
+      </h1>
+      <h3 className="tagline">
+        <span> {props.tagline} </span>
+      </h3>
+    </header>
+  );
+};
 
 export default Header;
