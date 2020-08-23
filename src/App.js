c774da148cc6008c4725a09cca2ba78e0db5cfed
possible-gratitude <abayomi.gratitudeindia@gmail.com>
2020-08-23 14:22:10 +0100

    template component created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./css/App.css";
+// import StorePicker from "./components/StorePicker";
+import Header from "./components/Header";
+import Order from "./components/Order";
+import Inventory from "./components/Inventory";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="catch-of-the-day">
+      <div className="menu">
+        <Header />
+      </div>
+      <Order />
+      <Inventory />
+      {/* <StorePicker /> */}
     </div>
   );
 }
